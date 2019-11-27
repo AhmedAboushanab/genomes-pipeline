0d192e4e7ad4ae48405c1b32fcc4a4c0f437e735
Ekaterina Sakharova <kates@ebi.ac.uk>
2019-11-27 16:39:18 +0000

    node.js

--- workflows/run.sh
+++ workflows/run.sh
@@ -2,6 +2,7 @@
 
 source /nfs/production/interpro/metagenomics/mags-scripts/annot-config
 source /hps/nobackup2/production/metagenomics/pipeline/tools-v5/toil-user-env/bin/activate
+export PATH=$PATH:/homes/emgpr/.nvm/versions/node/v12.10.0/bin/
 
 export MEMORY=20G
 export NUM_CORES=8
@@ -11,14 +12,14 @@ export OUT_DIR=/hps/nobackup2/production/metagenomics/pipeline/testing/kate_out
 export PIPELINE_FOLDER=/hps/nobackup2/production/metagenomics/databases/human-gut_resource/cwl_pipeline/genomes-pipeline
 
 export NAME_RUN=test-genomes
-export CWL=$PIPELINE_FOLDER/tools/checkm.cwl
-export YML=$PIPELINE_FOLDER/tools/checkm.yml
+export CWL=$PIPELINE_FOLDER/tools/checkm/checkm.cwl
+export YML=$PIPELINE_FOLDER/tools/checkm/checkm.yml
 
 # < set up folders >
 export JOB_TOIL_FOLDER=$WORK_DIR/$NAME_RUN/
 export LOG_DIR=${OUT_DIR}/logs_${NAME_RUN}
 export TMPDIR=${WORK_DIR}/global-temp-dir_${NAME_RUN}
-export $OUT_TOOL=${OUT_DIR}/{NAME_RUN}
+export OUT_TOOL=${OUT_DIR}/{NAME_RUN}
 
 mkdir -p $JOB_TOIL_FOLDER $LOG_DIR $TMPDIR $OUT_TOOL && \
 cd $WORK_DIR && \
@@ -32,5 +33,5 @@ time cwltoil \
   --outdir $OUT_TOOL \
   --logFile $LOG_DIR/${NAME_RUN}.log \
   --defaultCores $NUM_CORES \
-$CWL $YML > $OUT_TOOL/out1.json
+$CWL $YML
 
